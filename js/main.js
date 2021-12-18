c2c39c3e239cfe31bd275a30009ad001fe7da75d
Courtney McBeth <clm325@cornell.edu>
2021-12-18 18:56:42 -0500

    sizing image

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-var divOut = document.getElementById('graphicsBackground')
-
 let app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight,
